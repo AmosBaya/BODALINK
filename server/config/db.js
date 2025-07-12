7ca75a135668ba2b560dc3da86b56e142b482304
Amos Baya <amosbaya96@gmail.com>
2025-07-12 23:17:13 +0300

    feat(server): configured mongodb

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const ConnectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
+        process.exit(1);
+    }
+};
+
+module.exports = ConnectDB;
+
+
+
---
+++ server/server.js
@@ -0,0 +1,54 @@
+// importing required modules
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+// importing mongodb connection 
+const ConnectDB = require('./config/db');
+
+// consts
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// Log requests in development mode
+if (process.env.NODE_ENV === 'development') {
+  app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    console.log('We are in dev mode!');
+    next();
+  });
+}
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.statusCode || 500).json({
+    success: false,
+    error: err.message || 'Server Error',
+  });
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Promise Rejection:', err);
+  // Close server & exit process
+  process.exit(1);
+});
+
+
+// connect to db and start the server
+ConnectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
+
+module.exports = app;
